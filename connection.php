0aa86cd112714354a971c0ac917f654e0ad59300
Vaibhav160995 <vaibhavakbari55@gmail.co>
2025-03-30 12:29:43 -0400

    update data base

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 session_start();
 // include("mysqli-plugin.php");
 // error_reporting(~E_NOTICE || ~E_WARNING || ~E_ERROR);
-	$servername = "localhost";  
+	$servername = "127.0.0.1";  
 	$username = "root";  
-	$password = "123456";  
+	$password = "root";  
 	$db = "travel";  
 	global $con;
 	$con = mysqli_connect ($servername , $username , $password, $db) or die("unable to connect to host or database");  
