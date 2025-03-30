29bfbe0bd22a5f9c6c8f582bb4bc1da882cf9b0f
Vaibhav160995 <vaibhavakbari55@gmail.co>
2025-03-30 01:01:21 -0400

    Update All Files

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "http://localhost:8080/busspasssystem",
     supportFile: false,
     video: true,
     reporter: "mochawesome",
