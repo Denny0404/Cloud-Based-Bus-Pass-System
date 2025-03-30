8e350b4f1a28c1312665b8ce92368a832ebf8bf5
Vaibhav160995 <vaibhavakbari55@gmail.co>
2025-03-30 00:52:24 -0400

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
+    supportFile: false,
     video: true,
     reporter: "mochawesome",
     reporterOptions: {
