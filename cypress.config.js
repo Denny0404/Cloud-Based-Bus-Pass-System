8bb70930c6036e9c0373a547b5ba599c77b6fe55
Vaibhav160995 <vaibhavakbari55@gmail.co>
2025-03-30 23:28:52 -0400

    Reupdate yml

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://127.0.0.1:8080/busspasssystem",
+    baseUrl: "http://localhost:8080/busspasssystem",
     video: true,
     reporter: "mochawesome",
     reporterOptions: {
