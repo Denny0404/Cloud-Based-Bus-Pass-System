0b0f82652ee2f71001688da51ab00cf09fc08b1e
Vaibhav160995 <vaibhavakbari55@gmail.co>
2025-04-01 00:04:04 -0400

    Update Database

--- connection.php
+++ connection.php
@@ -2,9 +2,17 @@
 session_start();
 // include("mysqli-plugin.php");
 // error_reporting(~E_NOTICE || ~E_WARNING || ~E_ERROR);
-	$servername = "localhost";  
-	$username = "root";  
-	$password = "123456";  
+	// $servername = "localhost";  
+	// $username = "root";  
+	// $password = "123456";  
+	// $db = "travel";  
+	// global $con;
+	// $con = mysqli_connect ($servername , $username , $password, $db) or die("unable to connect to host or database");  
+
+
+	$servername = "blue-aardvark-631260.hostingersite.com";  
+	$username = "u342955335_travel";  
+	$password = "moneyInfo@7777";  
 	$db = "travel";  
 	global $con;
 	$con = mysqli_connect ($servername , $username , $password, $db) or die("unable to connect to host or database");  
